7f3c485f97accdf34aa5b373eaeda26dd98bf83d
Mehzabin Sadat Aothoi <mehzabin.sadat.aothoi@g.bracu.ac.bd>
2025-02-06 22:26:43 +0600

    Initial commit

---
+++ src/main/java/com/bumblebee/repairlog/RepairlogApplication.java
@@ -0,0 +1,13 @@
+package com.bumblebee.repairlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepairlogApplication.class, args);
+	}
+
+}
